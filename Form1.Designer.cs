a7e0f174d14884ae7b87a704606bbefa3feab5d0
hauntps <haunt.hcm2015@gmail.com>
2018-01-10 16:32:18 +0700

    Simple Compiler v1.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SimpleCompilerVersion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtFramework = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.RichTextBox();
+            this.txtDebug = new System.Windows.Forms.RichTextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtDebug);
+            this.panel1.Controls.Add(this.txtCode);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btnRun);
+            this.panel2.Controls.Add(this.txtFramework);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtOutput);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(810, 35);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(88, 8);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(165, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // txtFramework
+            // 
+            this.txtFramework.Location = new System.Drawing.Point(473, 8);
+            this.txtFramework.Name = "txtFramework";
+            this.txtFramework.Size = new System.Drawing.Size(36, 20);
+            this.txtFramework.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(379, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Framework";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(4, 54);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(809, 214);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.Text = "";
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(4, 289);
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.Size = new System.Drawing.Size(809, 104);
+            this.txtDebug.TabIndex = 2;
+            this.txtDebug.Text = "";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(272, 6);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 424);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Simple Compiler C#";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFramework;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.RichTextBox txtCode;
+        private System.Windows.Forms.RichTextBox txtDebug;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCompilerVersion2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            txtDebug.Clear();
+            CSharpCodeProvider provider 
+                = new CSharpCodeProvider(new Dictionary<string, string>()
+                    { {"compilerVersion", txtFramework.Text } });
+            CompilerParameters parameters 
+                = new CompilerParameters(new[] {"mscorlib.dll","System.Core.dll"},
+                    txtOutput.Text, true);
+
+            parameters.GenerateExecutable = true;
+            CompilerResults result = provider.CompileAssemblyFromSource(parameters, txtCode.Text);
+            if (result.Errors.HasErrors)
+            {
+                result.Errors.Cast<CompilerError>()
+                    .ToList()
+                    .ForEach(error => txtDebug.Text += error.ErrorText + "\r\n");
+            }
+            else
+            {
+                txtDebug.Text = "----- Build Succeed -----";
+                Process.Start(Application.StartupPath + "/" + txtOutput.Text);
+            }
+        }
+    }
+}
